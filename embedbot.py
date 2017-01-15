9ecc90dd7cd87cf33c55cf58db861e7ab696284d
Luigimaster1 <mariomario13245@gmail.com>
2017-01-15 16:48:10 -0400

    Added advanced mode, nick and some other commands.

--- embedbot.py
+++ embedbot.py
@@ -1,4 +1,4 @@
-# Embedbot 1.1 made by -Kiwi Catnip ♡#1540s
+# Embedbot 1.1 made by -Kiwi Catnip ♡#1540 and @isy#0669.
 
 #### -- CONFIG -- ###
 
@@ -25,12 +25,17 @@ password = asdf
 textargs = True
 #textargs = False
 
+### Advanced mode - ###
+# ONLY ENABLE THIS IF YOU ARE CAREFUL. This has the power to destroy your computer if you don't use it correctly.
+advancedmode = False
+
 # Don't touch below this line.
 
 import discord
 from discord.ext import commands
 import asyncio
 import inspect
+import os
 
 bot = commands.Bot(command_prefix=invoker, self_bot=True)
 
@@ -53,17 +58,33 @@ async def on_message(message):
     await bot.process_commands(message)
 			
 			
+@bot.command(pass_context=True)
+async def restart(ctx):
+    await bot.say("Restarting.")
+    await asyncio.sleep(1)
+    os.system(__file__)
+    await bot.logout()
+
+@bot.command(pass_context=True)
+async def nick(ctx):
+    cmdarg = ctx.message.content.split(" ",1)[1]
+    try:
+        await bot.change_nickname(ctx.message.server.me, cmdarg)
+        msg = await bot.say("Your nickname on this server has been changed to **{}**.".format(cmdarg))
+    except:
+        msg = await bot.say("Your nickname could not be changed on this server.")
+	
 @bot.command(pass_context=True, name="print")
 async def _print(ctx, asdf):
     print(asdf.encode("ascii","backslashreplace").decode())
 	
 @bot.command(pass_context=True)
 async def test(ctx):
-    await bot.say("The selfbot is active.")
+    await bot.say("The selfbot is active. no u")
 	
 @bot.command(pass_context=True)
 async def kill(ctx):
-    await bot.say("Killed. You can run the bot again by clicking on the file.")
+    await bot.say("Killed.")
     await asyncio.sleep(1)
     await bot.logout()
 
@@ -78,31 +99,43 @@ async def embeds(ctx, *, asdf):
             await bot.edit_message(ctx.message, "​", embed=em)
         else:
             await bot.say("I need the `embed links` permission to send an embed.")
-			
+
 @bot.command(pass_context=True, name='eval')
 async def _eval(ctx, *, code : str):
     """Evaluates code."""
-    code = code.strip('` ')
-    python = '```py\n{}\n```'
-    result = None
+    if advancedmode == True:
+        code = code.strip('` ')
+        python = '```py\n{}\n```'
+        result = None
 
-    env = {
-        'ctx': ctx,
-        'message': ctx.message,
-        'server': ctx.message.server,
-        'channel': ctx.message.channel,
-        'author': ctx.message.author
-    }
+        env = {
+            'ctx': ctx,
+            'message': ctx.message,
+            'server': ctx.message.server,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author
+        }
 
-    env.update(globals())
+        env.update(globals())
  
-    try:
-        result = eval(code, env)
-        if inspect.isawaitable(result):
-            result = await result
-    except Exception as e:
-        await bot.say(python.format(type(e).__name__ + ': ' + str(e)))
-        return
-    await bot.say(python.format(result))
+        try:
+            result = eval(code, env)
+            if inspect.isawaitable(result):
+                result = await result
+        except Exception as e:
+            await bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+            return
+        await bot.say(python.format(result))
+    else:
+        await bot.say("This command is an `advanced mode` command.")
 		
+		
+		
+# ----- Non useful commands ----- #
+
+@bot.command(pass_context=True)
+async def f(ctx):
+    await bot.edit_message(ctx.message, "`Respects have been paid.`")
+    await bot.add_reaction(ctx.message, '\U0001f1eb')
+	
 bot.run(email, password, bot=False)
