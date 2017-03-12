e82f17bad7040ba68832a29c43da59a1390e3c08
Luigimaster1 <mariomario13245@gmail.com>
2017-03-12 14:03:22 -0300

    installs dependencies

---
+++ instmod.py
@@ -0,0 +1,14 @@
+import pip
+import time
+print("Installing discord.py...")
+pip.main(['install', "discord.py"])
+print("Installing colorama...")
+pip.main(['install', "colorama"])
+print("Installing pillow...")
+pip.main(['install', "pillow"])
+print("Installing cursor...")
+pip.main(['install', "cursor"])
+print("Installing psutil...")
+pip.main(['install', "psutil"])
+print("Done.")
+time.sleep(5)
